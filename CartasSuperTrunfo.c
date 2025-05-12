5dc726197aa9f9ef675e0954152f33efbf8955a7
ButionDev <bution.dev@gmail.com>
2025-05-12 23:17:17 +0000

    Adicionado os requisitos "mestre"

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,40 +1,39 @@
-//Codigo criado no VSCode, usando apenas a extenção de C/C++ recomendada
-
 #include <stdio.h>
 
 int main(){
+
     
     //Definindo variáveis da primeira carta
-    char estado1[20];
     char cidade1[20];
-    char codigo1[10];
     int populacao1;
     float area1;
     float pib1;
     int pontos_turisticos1;
+    // Aventureiro: densidade populacional e pip per capta da primeira carta
+    float densisade_populacional1;
+    float pib_capta1;
+    // Mestre: Calculo de super poder, comparar cartas e exibir os resultados das comparações
+    float superpoder1;
     
     //Definindo variáveis da segunda cidade
-    char estado2[20];
     char cidade2[20];
-    char codigo2[20];
     int populacao2;
     float area2;
     float pib2;
     int pontos_turisticos2;
-
+    // Aventureiro: densidade populacional e pib per capta da segunda carta
+    float densisade_populacional2;
+    float pib_capta2;
+    // Mestre: Calculo de super poder, comparar cartas e exibir os resultados das comparações
+    float superpoder2;
 
     printf("Super Trunfo em C\n");
 
     //Solicitando atributos da primeira carta
 
-    printf("Qual é o nome do estado da primeira carta?\n");
-    scanf("%s", estado1);
-
     printf("Qual é o nome da cidade da primeira carta?\n");
     scanf("%s", cidade1);
 
-    printf("Qual será o codigo dessa carta?\n");
-    scanf("%s", codigo1);
   
     printf("Qual é a população dessa cidade?\n");
     scanf("%i", &populacao1);
@@ -51,12 +50,24 @@ int main(){
     //imprimindo atributos da primeira carta;
 
     printf("Carta %s\n", cidade1);
-    printf("Estado da carta: %s\n", estado1);
-    printf("Código da carta: %s\n", codigo1);
     printf("População: %i de pessoas\n", populacao1);
     printf("Area: %.2fKm²\n", area1);
     printf("PIB: %.2f\n", pib1);
     printf("Pontos turísticos: %i\n", pontos_turisticos1);
+
+    //Aventureiro:
+    densisade_populacional1 = (float) populacao1 / area1;
+
+    pib_capta1 = (float) pib1 / populacao1;
+
+    printf("Densidade populacional: %.2f hab/km²\n", densisade_populacional1);
+    printf("Pib per capta: %.2f reais\n", pib_capta1);
+
+    //mestre:
+    superpoder1 = (float) populacao1 + area1 + pib1 + (float) pontos_turisticos1
+    + densisade_populacional1 + pib_capta1;
+
+    printf("Super poder carta 1: %.2f (soma de todos os pontos)\n", superpoder1);
 
     //espaço para nova solicitação de dados
 
@@ -66,14 +77,8 @@ int main(){
     
     //Solicitando atributos da segunda carta
 
-    printf("Qual é o estado da segunda carta?\n");
-    scanf("%s", estado2);
-
     printf("Qual é o nome da segunda cidade?\n");
     scanf("%s", cidade2);
-
-    printf("qual é o codigo da segunda carta?\n");
-    scanf("%s", codigo2);
 
     printf("Qual é a população dessa cidade?\n");
     scanf("%i", &populacao2);
@@ -87,15 +92,29 @@ int main(){
     printf("Qual é a quantidade de pontos turístivos dessa segunda cidade?\n");
     scanf("%i", &pontos_turisticos2);
 
+    //Aventureiro:
+    densisade_populacional2 = (float) populacao2 / area2;
+
+    pib_capta2 = (float) pib2 / populacao2;
+
+
+    printf("Densidade populacional: %.2f hab/km²\n", densisade_populacional2);
+    printf("Pib per capta: %.2f reais\n", pib_capta2);
+
+        //mestre:
+    superpoder2 = (float) populacao2 + area2 + pib2 + (float) pontos_turisticos2
+    + densisade_populacional2 + pib_capta2;
+
+    printf("Super poder carta 2: %.2f (soma de todos os pontos)\n", superpoder2);
+
     //imprimindo atributos da segunda carta
 
     printf("Carta %s\n", cidade2);
-    printf("Estado da carta: %s\n", estado2);
-    printf("Código da carta: %s\n", codigo2);
     printf("População: %i de pessoas\n", populacao2);
     printf("Área: %.2fKm²\n", area2);
     printf("Pib: %.2f\n", pib2);
     printf("pontos turísticos: %i\n", pontos_turisticos2);
+    printf("Super poder carta 2: %.2f (soma de todos os pontos)\n", superpoder2);
 
     //dando um espaço para poder imprimir as duas cartas juntas
     printf(" \n");
@@ -108,12 +127,17 @@ int main(){
     printf("PRIMEIRA CARTA");
     printf(" \n");
     printf("Carta %s\n", cidade1);
-    printf("Estado da carta: %s\n", estado1);
-    printf("Código da carta: %s\n", codigo2);
     printf("População: %i de pessoas\n", populacao1);
     printf("Area: %.2fKm²\n", area1);
     printf("PIB: %.2f\n", pib1);
     printf("Pontos turísticos: %i\n", pontos_turisticos1);
+
+        //Aventureiro
+    printf("Densidade populacional: %.2f hab/km²\n", densisade_populacional1);
+    printf("Pib per capta: %.2f reais\n", pib_capta1);
+
+        //Mestre
+    printf("Super poder carta 1: %.2f (soma de todos os pontos)\n", superpoder1);
 
     printf(" \n");
     printf(" \n");
@@ -121,15 +145,32 @@ int main(){
     printf("SEGUNDA CARTA");
     printf(" \n");
     printf("Carta: %s\n", cidade2);
-    printf("Estado da carta: %s\n", estado2);
-    printf("Código da carta: %s\n", codigo2);
     printf("População: %i de pessoas\n", populacao2);
     printf("Área: %.2fKm²\n", area2);
     printf("Pib: %.2f\n", pib2);
     printf("pontos turísticos: %i\n", pontos_turisticos2);
 
+    //Aventureiro:
+    printf("Densidade populacional: %.2f hab/km²\n", densisade_populacional2);
+    printf("Pib per capta: %.2f reais\n", pib_capta2);
+
+    //Mestre:
+    printf("Super poder carta 2: %.2f (soma de todos os pontos)\n", superpoder2);
+
+    printf("\n");
+    printf("\n");
+
+    //Comparação das cartas e verificação da carta vencedora:
+
+    printf("Verificando qual é a carta vencedora, (1 = carta 1 venceu, 0 = carta 2 venceu)\n");
+    printf("População: %d\n", populacao1 > populacao2);
+    printf("Area: %d\n", area1 > area2);
+    printf("Pib: %d\n", pib1 > pib2);
+    printf("Pontos turisticos: %d\n", pontos_turisticos1 > pontos_turisticos2);
+    printf("Densidade populacional: %d\n", densisade_populacional1 > densisade_populacional2);
+    printf("Pib per capta: %d\n", pib_capta1 > pib_capta2);
+    printf("Super poder: %d\n", superpoder1 > superpoder2);
+
 
     return 0;
-
-
 }
